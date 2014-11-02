caeec3f8a145d721e57bc02011134b28fa7c6f0a
Okan Aşık <asik.okan@gmail.com>
2014-11-02 14:09:41 +0200

    changes in the launch file. revert camera.py to original state. change in camera_info yml

--- nodes/camera.py
+++ nodes/camera.py
@@ -149,13 +149,10 @@ class NaoCam (NaoNode):
 
             if new_config['source'] == kTopCamera:
                 self.frame_id = self.config['camera_top_frame']
-                self.config['camera_info_url'] = rospy.get_param('~calibration_file_top')
             elif new_config['source'] == kBottomCamera:
                 self.frame_id = self.config['camera_bottom_frame']
-                self.config['camera_info_url'] = rospy.get_param('~calibration_file_bottom')
             elif new_config['source'] == kDepthCamera:
                 self.frame_id = new_config['camera_depth_frame']
-                
             else:
                 rospy.logerr('Invalid source. Must be 0, 1 or 2')
                 exit(1)
@@ -194,7 +191,7 @@ class NaoCam (NaoNode):
                 if self.get_version() < LooseVersion('2.0'):
                     self.camProxy.setParam(naoqi_key, new_config[key])
                 else:
-                    self.camProxy.setCamerasParameter(self.nameId, naoqi_key, new_config[key])
+                    self.camProxy.setCameraParameter(self.nameId, naoqi_key, new_config[key])
 
         for key, naoqi_key, auto_exp_val in [('exposure', kCameraExposureID, 0),
                                              ('gain', kCameraGainID, 0), ('brightness', kCameraBrightnessID, 1)]:
@@ -203,16 +200,16 @@ class NaoCam (NaoNode):
                     self.camProxy.setParam(kCameraAutoExpositionID, auto_exp_val)
                     self.camProxy.setParam(naoqi_key, new_config[key])
                 else:
-                    self.camProxy.setCamerasParameter(self.nameId, kCameraAutoExpositionID, auto_exp_val)
-                    self.camProxy.setCamerasParameter(self.nameId, naoqi_key, new_config[key])
+                    self.camProxy.setCameraParameter(self.nameId, kCameraAutoExpositionID, auto_exp_val)
+                    self.camProxy.setCameraParameter(self.nameId, naoqi_key, new_config[key])
 
         if self.config['white_balance'] != new_config['white_balance'] or is_camera_new:
             if self.get_version() < LooseVersion('2.0'):
                 self.camProxy.setParam(kCameraAutoWhiteBalanceID, 0)
                 self.camProxy.setParam(kCameraWhiteBalanceID, new_config['white_balance'])
             else:
-                self.camProxy.setCamerasParameter(self.nameId, kCameraAutoWhiteBalanceID, 0)
-                self.camProxy.setCamerasParameter(self.nameId, kCameraWhiteBalanceID, new_config['white_balance'])
+                self.camProxy.setCameraParameter(self.nameId, kCameraAutoWhiteBalanceID, 0)
+                self.camProxy.setCameraParameter(self.nameId, kCameraWhiteBalanceID, new_config['white_balance'])
 
         key_methods =  [ ('resolution', 'setResolution'), ('color_space', 'setColorSpace'), ('frame_rate', 'setFrameRate')]
         if self.get_version() >= LooseVersion('2.0'):
@@ -267,9 +264,9 @@ class NaoCam (NaoNode):
             img.encoding = encoding
             img.step = img.width * nbLayers
             img.data = image[6]
-			
+
             self.pub_img_.publish(img)
-            
+
             # deal with the camera info
             if self.config['source'] == kDepthCamera and image[3] == kDepthColorSpace:
                 infomsg = CameraInfo()
@@ -300,7 +297,6 @@ class NaoCam (NaoNode):
                 self.pub_info_.publish(infomsg)
             elif self.config['camera_info_url'] in self.camera_infos:
                 infomsg = self.camera_infos[self.config['camera_info_url']]
-
                 infomsg.header = img.header
                 self.pub_info_.publish(infomsg)
 
